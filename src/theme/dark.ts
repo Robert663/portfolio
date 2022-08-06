dd7aeb751463d7c04f3f0c49dad66b76b286eff2
Robert <lober3464@gmail.com>
2022-08-06 15:26:39 -0300

    feat: adding different styles for Textfield and changing paper color

--- src/theme/dark.ts
+++ src/theme/dark.ts
@@ -1,21 +1,49 @@
 import { createTheme } from '@mui/material';
 import { cyan, orange } from '@mui/material/colors';
-
 export const Darktheme = createTheme({
   palette: {
     primary: {
       main: orange[500],
-      light:orange[300],
+      light: orange[300],
       dark: orange[700],
     },
-    secondary:{
-        main: cyan[800],
-        dark: cyan[100],
-        light: cyan[50],
+    secondary: {
+      main: cyan[800],
+      dark: cyan[100],
+      light: cyan[50],
     },
     background: {
-      paper: '#000',
+      paper: '#93acd5',
       default: '#202124',
+    },
+  },
+  components: {
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          border: 'solid 1px #fff', 
+          padding: '3px',
+          '& .MuiOutlinedInput-root': {
+
+            '& fieldset': {
+
+              borderColor: 'black', 
+            },
+            '&:hover fieldset': {
+              borderColor: 'red', 
+            },
+            '&.Mui-focused fieldset': {
+              borderColor: 'green',
+              
+            },
+          },
+        },
+      },
+    },
+  },
+  typography: {
+    allVariants: {
+      color: 'white',
     },
   },
 });
