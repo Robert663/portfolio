a47cab5259bf01021fc64bc82bd920284f858ad4
Robert <lober3464@gmail.com>
2022-08-06 15:26:16 -0300

    feat: adding and changing some routes

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,18 +1,19 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { AboutMe } from '../pages/AboutMe';
-import { Certificates } from '../pages/Certificates';
 import { Contact } from '../pages/Contact';
+import { Extra } from '../pages/Extra';
 import { Home } from '../pages/Home';
 import { Projects } from '../pages/Projects';
 
 export const Routers = () => {
   return (
     <Routes>
-      <Route path ="/home/" element ={<Home/>}/>
-      <Route path ="/sobre/" element ={<AboutMe/>}/>
-      <Route path ="/certificados/" element ={<Certificates/>}/>
-      <Route path ="/projetos/" element ={<Projects/>}/>
-      <Route path ="/contato/" element ={<Contact/>}/>
+      <Route path="/" element={<Navigate to="/home/" />} />
+      <Route path="/home/" element={<Home />} />
+      <Route path="/sobre/" element={<AboutMe />} />
+      <Route path="/projetos/" element={<Projects />} />
+      <Route path="/contato/" element={<Contact />} />
+      <Route path="/extra/" element={<Extra />}/>
     </Routes>
   );
 };
